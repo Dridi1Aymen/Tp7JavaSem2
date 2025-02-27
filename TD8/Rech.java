9da6d5d2e5c2ba821692e69ba401febeffd9e688
Aymen Dridi <aymendridi2002@gmail.com>
2025-02-27 15:49:32 +0100

    TD8 enrigistre

---
+++ TD8/Rech.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Rech {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String mot1,mot2;
+        int L,P;
+        System.out.println("saisir un text");
+        text=sc.next();
+        System.out.println("saisir un mot1");
+        mot1=sc.next();
+        System.out.println("saisir un mot2");
+        mot2=sc.next();
+        
+
+    }
+
+    
+}
